1fc2e88606f6f38bf4276002ab9283fed08dc47d
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-11 10:18:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Manderley::Application.config.secret_key_base = 'd1f9a83f12f148cc38a8ee90a9e8a6029d234d72946213f24f7c64bd76b988317388928d1e55e2917063f17113f322397e23cf5f87b97bca9d92234f1fd45526'
