144cc485b37ce8b3e76406735640cbd6e94cd482
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-11 13:05:58 +0100

    Configuring app

---
+++ app/views/movies/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @movie, :title, :duration, :created_at, :updated_at
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ Bundler.require(:default, Rails.env)
 
 module Manderley
   class Application < Rails::Application
+    config.generators do |g|
+      g.view_specs false    
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
