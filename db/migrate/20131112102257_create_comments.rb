34fb02b160b4d186d2c6d8cc50499a6a10063734
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-12 11:27:26 +0100

    Created fieds in comment migration

--- db/migrate/20131112102257_create_comments.rb
+++ db/migrate/20131112102257_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string    :author
+      t.text      :body
+      t.references  :movie, index: true
 
       t.timestamps
     end
