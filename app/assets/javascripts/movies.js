afdb8d4d01254368ea0ca8132e5bc826496cfa68
repli <repli@MacBook-Pro-de-repli.local>
2013-11-14 22:07:54 +0100

    jQuery add person form

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,7 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-$('#add-comment').on('click', function(event) {
-    $('.comment-form').css('display', 'block')
-});    
+    $('#add-comment').on('click', function(event) {
+        $('.comment-form').css('display', 'block');
+    });    
 })
+
+
+
+$(document).ready(function() {
+    $('#add-person').on('click', function(event) {
+       $('.cast-form').css('display', 'block');
+    });
+});
