04aaa2193fb8d85243073056a6ca60b90e676e22
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-15 17:33:10 +0100

    Added movie scopes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,10 @@ class Movie < ActiveRecord::Base
   has_many :people, through: :casts
 
   validates_presence_of :title, :year, :duration
+
+  scope :short, -> { where('duration < ?', 60) }
+  scope :longer_that, ->(duration) {  where('duration > ?',duration) }
+  scope :nineties, -> { where('? < year < ?', 1989, 2000) }
+  scope :modern, -> { where('year > ?', 2000) }
+  scope :gorgeous, -> { self.modern.long }
 end
--- scripts/load_jw.rb
+++ scripts/load_jw.rb
@@ -1,22 +1,31 @@
 require 'json'
-
-
-
-def create_movie_from_json
-  Movie.create title: json["Title"], duration: json["Duration"], year: json["Year"], synopsis: json["Synopsis"]
-end
-def load_movies_from_file(file)
-    File.foreach(file) do |l|
-      unless l.blank?
-        json = JSON.parse(l)
-        create_movie_from_json(json)
-      end
+ 
+ 
+def to_minutes b
+  if b== "N/A"
+    0
+  elsif b.index('h')
+    b.to_i*60 + b[b.index('h')..-1].to_i
+  else
+    b.to_i
   end
 end
 
-
+def create_movie_from_json json
+  Movie.create title: json["Title"], duration: to_minutes(json["Runtime"]), year: json["Year"], synopsis: json["Plot"], categories: json["Genre"]
+end
+ 
+def load_movies_from_file(file)
+  File.foreach(file) do |l|
+    unless l.blank?
+      json = JSON.parse(l)
+      create_movie_from_json(json)
+    end
+  end
+end
+ 
 def load_default_file
   current_dir = File.dirname(__FILE__)
-  file = File.join(current_dir, "jw.json")
+  file = File.join(current_dir, 'jw.json')
   load_movies_from_file file
 end
