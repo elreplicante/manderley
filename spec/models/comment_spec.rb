cdf78337613ce10e3e70d75f46da08a3d22dfb77
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-21 00:27:01 +0100

    Added comments specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "with valid attributes" do
+    it "should be valid" do
+      expect(build(:comment)).to be_valid
+    end
+  end
+
+  describe "is invalid" do
+    it "without a body" do
+      expect(build(:comment, body: nil)).to_not be_valid
+    end
+
+    it "without a movie" do
+      expect(build(:comment, movie: nil)).to_not be_valid
+    end
+
+    it "with an body bigger than 500 characters" do
+      expect(build(:comment, body: 'h' * 501 )).to_not be_valid
+    end
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "with valid attributes" do
+    xit "should be valid" do
+      expect(build(:person)).to be_valid
+    end
+  end
 end
