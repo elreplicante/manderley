91eefefb51b2a296f3c861492cf18f0aba273c8d
Sergio Revilla <sergio@unmundotecnologico.org>
2013-11-20 23:44:45 +0100

    Added movie model specs

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "with valid attributes" do
+    it "should be valid" do
+      expect(build(:movie)).to be_valid
+    end
+  end
+
+  describe "is invalid" do
+    it "without a title" do
+      expect(build(:movie, title: nil)).to_not be_valid
+    end
+
+    it "without a year" do
+      expect(build(:movie, year: nil)).to_not be_valid
+    end
+
+    it "without a duration" do
+      expect(build(:movie, duration: nil)).to_not be_valid
+    end
+  end
 end
