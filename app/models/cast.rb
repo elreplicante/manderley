eef1cf362443c78639299ecf5dedd65dde1f93e7
Sergio Revilla <sergio@unmundotecnologico.org>
2013-12-02 10:20:20 +0100

    Added directors scope to Movie model

--- app/models/cast.rb
+++ app/models/cast.rb
@@ -1,4 +1,4 @@
-  class Cast < ActiveRecord::Base
+class Cast < ActiveRecord::Base
   belongs_to :movie
   belongs_to :person
   validates :movie_id, :uniqueness => { :scope => [:person_id, :role] }
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,14 +5,16 @@ class Movie < ActiveRecord::Base
   has_many :people, through: :casts
   belongs_to :user
 
+  has_many :directors, -> { Cast.where(role: 'director') }, through: :casts, source: :person
   validates_presence_of :title, :year, :duration
 
   scope :short, -> { where('duration < ?', 60) }
-  scope :longer_that, ->(duration) {  where('duration > ?',duration) }
+  scope :longer_that, ->(duration) {  where('duration > ?', duration) }
   scope :medium, -> { longer_that 60 }
   scope :long, -> { longer_that 90 }
   scope :huge, -> { longer_that 120 }
   scope :nineties, -> { where('? < year < ?', 1989, 2000) }
   scope :modern, -> { where('year > ?', 2000) }
   scope :gorgeous, -> { self.modern.long }
+  scope :last_at_least, ->(n) {  }
 end
